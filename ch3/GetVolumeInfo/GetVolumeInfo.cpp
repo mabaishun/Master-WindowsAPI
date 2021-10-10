a2e0b8b64bde91a082ee212c96ae5fd4d35b63ca
spider <45061651+mabaishun@users.noreply.github.com>
2021-10-10 23:09:33 +0800

    Windows API
    精通Windows API学习笔记

---
+++ ch3/GetVolumeInfo/GetVolumeInfo.cpp
@@ -0,0 +1,18 @@
+// GetVolumeInfo.cpp 编译驱动器并获取驱动器属性
+
+//头文件
+#include <Windows.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+//预定义
+#define BUFSIZE 1024
+
+//函数声明
+BOOL GetDirverInfo(LPSTR szDrive);
+
+//遍历驱动器并调用GetDriverInfo，获取驱动器属性
+int main(void)
+{
+	return 0;
+}
